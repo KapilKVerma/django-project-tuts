6404a9437ab726dc381a2a85b4a095cc42588c6a
KapilKVerma <kapilkumaarverma@gmail.com>
2020-12-19 14:23:13 +1100

    Added models and Views

--- djangotutorial/urls.py
+++ djangotutorial/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pages.views import home_view, about_view, contact_view
+from products.views import product_detail_view
 
 urlpatterns = [
+    path("", home_view, name='home'),
+    path("contact/", contact_view, name='contact'),
+    path("about/", about_view, name='about'),
+    path("item/", product_detail_view, name='item'),
     path('admin/', admin.site.urls),
 ]
